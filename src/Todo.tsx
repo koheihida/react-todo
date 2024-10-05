f364130cd6c9979b4c2417bfff7afeda4c8b780d
koheihida <koheihida04@gmail.com>
2024-10-06 00:14:45 +0900

    HTMLの実装

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -4,9 +4,40 @@ import './App.css';
 
 export const Todo = () =>{
   return (
-    <div>
-      Preparation
-    </div>
+    <>
+      <div>
+        <input placeholder='TODOを入力！！' />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>
+            <p>TODOです</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+          <li>
+            <p>TODOです</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+      <div>
+      <p>完了のTODO</p>
+        <ul>
+          <li>
+            <p>TODOです</p>
+            <button>戻す</button>
+          </li>
+          <li>
+            <p>TODOです</p>
+            <button>戻す</button>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
 
