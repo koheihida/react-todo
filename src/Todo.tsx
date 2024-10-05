19e8ecb9048ff4158fe91ef518864b69d0d15ac6
koheihida <koheihida04@gmail.com>
2024-10-05 23:40:05 +0900

    Preparation commit

---
+++ src/Todo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export const Todo = () =>{
+  return (
+    <div>
+      Preparation
+    </div>
+  );
+}
+
+export default Todo;
